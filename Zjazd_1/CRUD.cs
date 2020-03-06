da99fcefa99f42fa295fa02a9640e2b9a5948b75
ArcyAs <serilmagnifico@gmail.com>
2020-03-06 17:45:22 +0100

    ver1

--- Zjazd_1/CRUD.cs
+++ Zjazd_1/CRUD.cs
@@ -7,93 +7,45 @@ using System.Data.SqlClient;
 namespace Zjazd_1
 
 {
-
     public class CRUD
-
     {
-
         public void Create(int id, string regionDescription, SqlConnection conn)
-
         {
-
             string sql = "INSERT INTO Region(RegionID, 'RegionDescription') VALUES(@regionName)";
-
             var command = new SqlCommand(sql, conn);
-
             command.Parameters.AddWithValue("regionName", regionDescription);
-
             command.Parameters.AddWithValue("id", id);
-
             int x = command.ExecuteNonQuery();
-
             if (x>0)
-
             {
-
                 Console.WriteLine("Wpisano");
-
             }
-
         }
-
- 
-
         public void Read(SqlConnection conn)
-
         {
-
             SqlCommand command = new SqlCommand
-
             {
-
                 CommandText = "SELECT * FROM Customers",
-
                 Connection = conn
-
             };
-
             SqlDataReader reader = command.ExecuteReader();
-
             while (reader.Read())
-
             {
-
                 Console.WriteLine(reader["CompanyName"]);
-
             }
-
         }
-
- 
-
         public void Update(int id, string NewName, SqlConnection conn)
-
         {
-
             string sql = "UPDATE Region WHERE id = @id SET RegionDescription = @regionName";
-
             var command = new SqlCommand(sql, conn);
-
             command.Parameters.AddWithValue("id", id);
-
             command.Parameters.AddWithValue("regionName", newName);
-
- 
-
             int x = command.ExecuteNonQuery();
-
             if (x>0)
-
             {
-
                 Console.WriteLine("Zmieniono");
-
             }
-
         }
-
-
-
         public void Delete();
 
     }
--- Zjazd_1/Program.cs
+++ Zjazd_1/Program.cs
@@ -5,53 +5,28 @@ using System.Data.SqlClient;
 namespace Zjazd_1
 
 {
-
     class Program
-
     {
-
         static void Main(string[] args)
-
         {
-
             string connectionString = ;
-
             using SqlConnection conn = new SqlConnection(connectionString);
-
-
-
             conn.Open();
-
             SqlCommand command = new SqlCommand
-
             {
-
                 CommandText = "SELECT * FROM Customers",
-
                 Connection = conn
-
             };
-
             SqlDataReader reader = command.ExecuteReader();
-
             while (reader.Read())
-
             {
-
                 Console.WriteLine(reader["CompanyName"]);
-
             }
-
             CRUD crud = new CRUD();
-
             crud.Read(conn);
-
             crud.Create(5, "Slask", conn);
-
             crud.conn.Close();
-
         }
-
     }
 }
     
